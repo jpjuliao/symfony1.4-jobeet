061872e5f0db0210293134055b2e9238725f5c75
JP Juliao <jpjuliao@gmail.com>
2024-05-16 15:54:44 -0500

    Setup Job routes

--- apps/frontend/modules/job/templates/indexSuccess.php
+++ apps/frontend/modules/job/templates/indexSuccess.php
@@ -1,11 +1,18 @@
 <h1>Jobeet jobs List</h1>
 <div id="jobs">
   <table class="jobs">
-    <?php foreach ($jobeet_jobs as $i => $job): ?>
+    <?php foreach ($jobeet_jobs as $i => $job) : ?>
       <tr class="<?php echo fmod($i, 2) ? 'even' : 'odd' ?>">
         <td class="location"><?php echo $job->getLocation() ?></td>
         <td class="position">
-          <a href="<?php echo url_for('job/show?id='.$job->getId()) ?>">
+          <a href="<?php echo url_for(array(
+                      'module'   => 'job',
+                      'action'   => 'show',
+                      'id'       => $job->getId(),
+                      'company'  => $job->getCompany(),
+                      'location' => $job->getLocation(),
+                      'position' => $job->getPosition(),
+                    )) ?>">
             <?php echo $job->getPosition() ?>
           </a>
         </td>
