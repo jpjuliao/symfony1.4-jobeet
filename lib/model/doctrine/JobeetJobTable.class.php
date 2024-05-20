e317c6ed46649c7f6f2d3f3aaeb8fa5e0bebafe8
JP Juliao <jpjuliao@gmail.com>
2024-05-20 11:38:17 -0500

    Sort jobs by category and then by date

--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -4,9 +4,13 @@ class JobeetJobTable extends Doctrine_Table
 {
   public function getActiveJobs()
   {
-    $q = $this->createQuery('j')
-      ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()))
-      ->orderBy('j.expires_at DESC');
+    if (is_null($q)) {
+      $q = Doctrine_Query::create()
+        ->from('JobeetJob j');
+    }
+
+    $q->andWhere('j.expires_at > ?', date('Y-m-d H:i:s', time()))
+      ->addOrderBy('j.expires_at DESC');
 
     return $q->execute();
   }
