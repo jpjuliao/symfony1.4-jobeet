ab99150a030cf34f685ca08aff9d82a83a1b47e6
JP Juliao <jpjuliao@gmail.com>
2024-05-20 11:16:20 -0500

    Refactor to move business logic to the model, define getActiveJobs method. Code is now unit testable

--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -2,4 +2,11 @@
 
 class JobeetJobTable extends Doctrine_Table
 {
+  public function getActiveJobs()
+  {
+    $q = $this->createQuery('j')
+      ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()));
+
+    return $q->execute();
+  }
 }
