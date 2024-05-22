3ab185e1d056ab0e5fda5b908941c8c5be3df266
JP Juliao <jpjuliao@gmail.com>
2024-05-22 14:50:23 -0500

    Add unit test for slugify feature

--- lib/model/doctrine/Jobeet.class.php
+++ lib/model/doctrine/Jobeet.class.php
@@ -6,9 +6,11 @@ class Jobeet
   {
     // replace all non letters or digits by -
     $text = preg_replace('/\W+/', '-', $text);
-
+    
     // trim and lowercase
     $text = strtolower(trim($text, '-'));
+    
+    if (empty($text)) return 'n-a';
 
     return $text;
   }
