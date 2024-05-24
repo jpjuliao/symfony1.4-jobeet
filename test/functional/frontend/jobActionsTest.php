25e0bf0b365a1f0fa9724c7dc0d91e0a7571a282
JP Juliao <jpjuliao@gmail.com>
2024-05-24 07:47:19 -0500

    Add Job functional test

--- test/functional/frontend/jobActionsTest.php
+++ test/functional/frontend/jobActionsTest.php
@@ -1,19 +1,36 @@
 <?php
 
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
-
-$browser = new sfTestFunctional(new sfBrowser());
-
-$browser->
-  get('/job/index')->
-
+ 
+$browser = new JobeetTestFunctional(new sfBrowser());
+$browser->loadData();
+ 
+$browser->info('1 - The homepage')->
+  get('/')->
   with('request')->begin()->
     isParameter('module', 'job')->
     isParameter('action', 'index')->
   end()->
-
   with('response')->begin()->
-    isStatusCode(200)->
-    checkElement('body', '!/This is a temporary page/')->
+    info('  1.1 - Expired jobs are not listed')->
+    checkElement('.jobs td.position:contains("expired")', false)->
+  end()
+;
+
+$max = sfConfig::get('app_max_jobs_on_homepage');
+ 
+$browser->info('1 - The homepage')->
+  get('/')->
+  info(sprintf('  1.2 - Only %s jobs are listed for a category', $max))->
+  with('response')->
+    checkElement('.category_programming tr', $max)
+;
+
+$browser->info('1 - The homepage')->
+  get('/')->
+  info('  1.3 - A category has a link to the category page only if too many jobs')->
+  with('response')->begin()->
+    checkElement('.category_design .more_jobs', false)->
+    checkElement('.category_programming .more_jobs')->
   end()
 ;
