0c5ace254662715a7c50e851f4bf17da84b55828
JP Juliao <jpjuliao@gmail.com>
2024-05-27 17:12:00 -0500

    Add forms

--- apps/frontend/modules/job/templates/showSuccess.php
+++ apps/frontend/modules/job/templates/showSuccess.php
@@ -1,8 +1,8 @@
 <?php use_helper('Text') ?>
 <?php slot('title') ?>
-  <?php echo sprintf('%s is looking for a %s', $job->getCompany(), $job->getPosition()) ?>
+<?php echo sprintf('%s is looking for a %s', $job->getCompany(), $job->getPosition()) ?>
 <?php end_slot() ?>
- 
+
 <div id="job">
   <h1><?php echo $job->getCompany() ?></h1>
   <h2><?php echo $job->getLocation() ?></h2>
@@ -10,31 +10,31 @@
     <?php echo $job->getPosition() ?>
     <small> - <?php echo $job->getType() ?></small>
   </h3>
- 
-  <?php if ($job->getLogo()): ?>
+
+  <?php if ($job->getLogo()) : ?>
     <div class="logo">
       <a href="<?php echo $job->getUrl() ?>">
-        <img src="/uploads/jobs/<?php echo $job->getLogo() ?>"
-          alt="<?php echo $job->getCompany() ?> logo" />
+        <img src="/uploads/jobs/<?php echo $job->getLogo() ?>" alt="<?php echo $job->getCompany() ?> logo" />
       </a>
     </div>
   <?php endif ?>
- 
+
   <div class="description">
     <?php echo simple_format_text($job->getDescription()) ?>
   </div>
- 
+
   <h4>How to apply?</h4>
- 
+
   <p class="how_to_apply"><?php echo $job->getHowToApply() ?></p>
- 
+
   <div class="meta">
     <small>posted on <?php echo $job->getDateTimeObject('created_at')->format('m/d/Y') ?></small>
   </div>
- 
+
   <div style="padding: 20px 0">
-    <a href="<?php echo url_for('job/edit?id='.$job->getId()) ?>">
-      Edit
-    </a>
+    <a href="<?php echo url_for('job_edit', $job) ?>">Edit</a>
+    <?php if ($sf_request->getParameter('token') == $job->getToken()) : ?>
+      <?php include_partial('job/admin', array('job' => $job)) ?>
+    <?php endif ?>
   </div>
 </div>
